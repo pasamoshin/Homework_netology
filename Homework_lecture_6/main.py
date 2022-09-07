17a2be0a435ce5122a4964f683235bd724fb56ff
Pavel Samoshin <pasamsohin@gmail.com>
2022-09-08 00:33:00 +0300

    test commit

--- Homework_lecture_6/main.py
+++ Homework_lecture_6/main.py
@@ -1,2 +1,16 @@
 # Homework for lecture 6
-test
+# Task 1: Assistance to the secretary
+
+print('Task 1: Assistance to the secretary')
+
+documents = [
+    {"type": "passport", "number": "2207 876234", "name": "Василий Гупкин"},
+    {"type": "invoice", "number": "11-2", "name": "Геннадий Покемонов"},
+    {"type": "insurance", "number": "10006", "name": "Аристарх Павлов"}
+]
+
+directories = {
+    '1': ['2207 876234', '11-2', '5455 028765'],
+    '2': ['10006'],
+    '3': []
+}
